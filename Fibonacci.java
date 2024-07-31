46cc8524619a4aaf0d1d20035479e2dfdb65d456
mustyuksel <mustafayukselburhaniye@gmail.com>
2024-07-31 14:23:17 +0300

    Fibonacci Finalized

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,31 +1,42 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Fibonacci dizesi'nin öğrenmek istediğiniz sırasını yazınız");
-     int line1 = sc.nextInt();
-        System.out.println(line1 +" sırasında terim değeri = "+ findFibonacci(line1 ));
+        int line1=0;
+        try{
+            line1 = sc.nextInt();
+        }
+        catch (Exception e){
+            System.err.println("Girdiğiniz değer geçersizdir.");
+            return;
+        }
+        sc.close();
+            if(line1<=0){
+                System.out.println("Girdiğiniz değer geçersizdir.");
+            }
+            else {
+                System.out.println(line1 + " sırasında terim değeri = " + findFibonacci(line1));
+            }
     }
 
     public static int findFibonacci(int line1) {
-        int ilkTerim = 0;
-        int ikinciTerim = 1;
-        int sonrakiTerim = ilkTerim + ikinciTerim;
-        if (line1 == 1) {
+            int ilkTerim = 0;
+            int ikinciTerim = 1;
+            int sonrakiTerim =0;
+            if (line1 == 1) {
             return ilkTerim;
-        }
-        if(line1==2){
+            }
+            if (line1 == 2) {
             return ikinciTerim;
-        }
-        if(line1==3){
-            return ikinciTerim + ilkTerim;
-        }
-        for (int i = 0; i < line1-3; i++) {
-            ilkTerim = ikinciTerim;
-            ikinciTerim = sonrakiTerim;
-            sonrakiTerim = ilkTerim + ikinciTerim;
-        }
+            }
+            for (int i = 1; i < line1 ; i++) {
+                ilkTerim = ikinciTerim;
+                ikinciTerim = sonrakiTerim;
+                sonrakiTerim = ilkTerim + ikinciTerim;
+            }
             return sonrakiTerim;
-    }
+        }
+
 }
