5e6ed4c18cd4385f97463d0e1e6d723faca0751e
mustyksel <mustafayukselburhaniye@gmail.com>
2024-08-11 15:38:00 +0300

    Unique Finalized

--- Unique.java
+++ Unique.java
@@ -4,7 +4,20 @@ public class Unique {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bir kelime yazın ve benzersiz mi değil mi öğrenin.");
-        String line1 = sc.nextLine();
+        String line1 ="";
+        try{
+            line1 = sc.nextLine();
+            if(!line1.matches("[a-zA-Z]+")){
+                throw new Exception();
+            }
+        }
+        catch (Exception e){
+            System.err.println("Geçersiz kelime tekrar deneyiniz ");
+            main(args);
+            sc.close();
+            return;
+        }
+        sc.close();
         String lowerLine = line1.toLowerCase();
         String Line1[] = lowerLine.split("");
         if(findUnique(Line1)){
@@ -17,17 +30,13 @@ public class Unique {
     public static boolean findUnique(String[] Line1){
         int counter =0;
         for (int i = 0; i < Line1.length; i++) {
-            int a = 0;
-            for (a = 0; a < Line1.length; a++) {
+            for (int a = 0; a < Line1.length; a++) {
                 if(i==a){
                     continue;
                 }
-                    if (Line1[i].equals(Line1[a])) {
-                        counter++;
-                    }
-                    else {
-                        continue;
-                    }
+                if (Line1[i].equals(Line1[a])) {
+                    counter++;
+                }
             }
         }
         if(counter>0){
